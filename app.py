be79629e059efe97a0f91f49cdc7b9cb69b93d0c
VADLAPUDIOMPRAKASH <130260642+VADLAPUDIOMPRAKASH@users.noreply.github.com>
2025-03-28 18:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ def analyze_video():
         return jsonify({'error': f"An error occurred: {str(e)}"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render uses 10000 by default
+    app.run(host="0.0.0.0", port=port)
